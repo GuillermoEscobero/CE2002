80bb6f4e05a678ce1168fbf98b2219a2450a36ec
Guillermo Escobero <100346060@alumnos.uc3m.es>
2019-02-22 02:31:46 +0800

    Progress lab4

---
+++ lab4/SalePerson.java
@@ -0,0 +1,55 @@
+public class SalePerson implements Comparable<SalePerson> {
+    private String firstName;
+    private String lastName;
+    private int totalSales;
+
+    public SalePerson(String firstName, String lastName, int totalSales) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.totalSales = totalSales;
+    }
+
+    public String toString() {
+        return lastName + ", " + firstName + " : " + totalSales;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if ((o == null) || (o.getClass() != this.getClass()))
+            return false;
+        SalePerson salePerson = (SalePerson)o;
+
+        return (firstName == salePerson.firstName || (firstName != null && firstName.equals(salePerson.firstName))) &&
+               (lastName == salePerson.lastName || (lastName != null && lastName.equals(salePerson.lastName)));
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + (null == firstName ? 0 : firstName.hashCode());
+        hash = 31 * hash + (null == lastName ? 0 : lastName.hashCode());
+        return hash;
+    }
+
+    @Override
+    public int compareTo(SalePerson o) {
+        int result;
+        result = Integer.compare(totalSales, o.totalSales);
+        //result = (result == 0 ? 0 : lastName.compareTo(o.lastName));
+        return result;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public int getTotalSales() {
+        return this.totalSales;
+    }
+}
